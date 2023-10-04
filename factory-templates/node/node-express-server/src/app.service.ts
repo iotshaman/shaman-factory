9bb0d39b437c4a3d2b4262b3230789de0f72592b
Mason Kleinsorge <Kleinsorgem97@outlook.com>
2023-10-04 16:40:15 -0500

    updating node-express-server to use shaman-api

--- factory-templates/node/node-express-server/src/app.service.ts
+++ factory-templates/node/node-express-server/src/app.service.ts
@@ -1,19 +1,29 @@
-/* istanbul ignore file */
-import { Container } from 'inversify';
+import "reflect-metadata";
+import * as _path from 'path';
+import { ConfigFactory, ShamanExpressApp } from 'shaman-api';
 
-import { Configure } from './composition/app.composition';
-import { TYPES } from './composition/app.composition.types';
-import { ConfigFactory } from './config.factory';
-import { IApiService } from './api.service';
+import { AppConfig } from "./models/app.config";
+import { Compose } from "./composition/app.composition";
 
-ConfigFactory.GenerateConfig()
-  .then(config => Configure(config))
-  .then((container: Container) => {
-    let apiService = container.get<IApiService>(TYPES.ApiService);
-    apiService.configure(container);
-    return apiService.startApplication();
-  })
-  .catch(ex => {
-    console.dir(ex);
-    process.exit(1);
+let bootstrap = async () => {
+  let configPath = _path.join(__dirname, '..', 'app', 'config', 'app.config.json');
+  const config = await ConfigFactory.GenerateConfig<AppConfig>(configPath);
+  const app = new ShamanExpressApp({
+    configPath: configPath,
+    port: parseInt(config.port),
+    headerAllowList: [
+      'Content-Type',
+      'Data-Type',
+      'Authorization'
+    ]
   });
+  let container = await app.compose();
+  await Compose(container);
+  await app.configureRouter([]);
+  await app.startApplication();
+}
+
+bootstrap().catch(ex => {
+  console.error(ex);
+  process.exit(1);
+});
--- factory-templates/node/node-express-server/src/composition/app.composition.types.ts
+++ factory-templates/node/node-express-server/src/composition/app.composition.types.ts
@@ -1,16 +1,8 @@
 const TYPES = {
   AppConfig: "AppConfig",
   Logger: "Logger",
-  ApiService: "ApiService",
-  ExpressApplication: "ExpressApplication",
-  ApiRouter: "ApiRouter",
   JsonFileService: "JsonFileService",
   //shaman: {"lifecycle": "transformation", "args": {"type": "compose", "target": "TYPES"}}
 };
 
-const CONTROLLER_TYPES = {
-  HealthController: "HealthController",
-  //shaman: {"lifecycle": "transformation", "args": {"type": "compose", "target": "CONTROLLER_TYPES"}}
-}
-
-export { TYPES, CONTROLLER_TYPES };
+export { TYPES };
